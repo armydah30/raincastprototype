8193f1246f0afb9cd83db8a55ef05ac3f2e33c00
armydah30 <hameedatomoine@gmail.com>
2018-05-19 12:40:51 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, request, flash, redirect, url_for, session, logging
+#from data import Articles
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
+from functools import wraps
+
+app = Flask(__name__)
+app.debug = True
+
+#config mySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'sha256user'
+app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_DB'] = 'mrmeteo'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+#init mySQL
+mysql = MySQL(app)
+
+
+
+
+#Index
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+#About
+@app.route('/about')
+def about():
+    return render_template('home.html')
+
+
+#Meteodata
+@app.route('/weatherdata')
+def weatherdata():
+    return render_template('weatherdata.html')
+
+
+@app.route('/vxml')
+def vxml():
+    return render_template('test.xml')
+
+
+
+if __name__ == '__main__':
+    app.secret_key='secret123'
+    app.run()
