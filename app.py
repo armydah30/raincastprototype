994eaee285bf312a3cf8bbf196ba0fa2112b0ec0
armydah30 <hameedatomoine@gmail.com>
2018-05-19 13:59:49 +0200

    first commit

--- app.py
+++ app.py
@@ -1,23 +1,11 @@
-from flask import Flask, render_template, request, flash, redirect, url_for, session, logging
-#from data import Articles
-from flask_mysqldb import MySQL
-from wtforms import Form, StringField, TextAreaField, PasswordField, validators
-from passlib.hash import sha256_crypt
+from flask import Flask, render_template, request, g, flash, redirect, url_for, session, logging
 from functools import wraps
 import os
 
 app = Flask(__name__)
 app.debug = False
 
-#config mySQL
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'sha256user'
-app.config['MYSQL_PASSWORD'] = 'password'
-app.config['MYSQL_DB'] = 'mrmeteo'
-app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
-#init mySQL
-mysql = MySQL(app)
 
 
 
@@ -25,11 +13,6 @@ mysql = MySQL(app)
 #Index
 @app.route('/')
 def index():
-    return render_template('home.html')
-
-#About
-@app.route('/about')
-def about():
     return render_template('home.html')
 
 
@@ -43,8 +26,13 @@ def weatherdata():
 def vxml():
     return render_template('test.xml')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 
 
 if __name__ == '__main__':
+
 	port = int(os.environ.get('PORT', 5000)) #The port to be listening to — hence, the URL must be <hostname>:<port>/ inorder to send the request to this program
 	app.run(host='0.0.0.0', port=port)#Start listening
