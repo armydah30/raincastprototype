2dbb92f74b2a0e7c9fb7d3688c304c4a825f1856
armydah30 <hameedatomoine@gmail.com>
2018-05-24 12:56:42 +0200

    complete app

--- app.py
+++ app.py
@@ -2043,6 +2043,6 @@ def bolgatanga2():
 
 
 if __name__ == '__main__':
-    #app.run()
+    #app.run() --for localhost
 	port = int(os.environ.get('PORT', 5000)) #The port to be listening to — hence, the URL must be <hostname>:<port>/ inorder to send the request to this program
 	app.run(host='0.0.0.0', port=port)#Start listening
